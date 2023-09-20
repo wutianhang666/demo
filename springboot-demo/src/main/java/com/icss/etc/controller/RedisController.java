54fc50c1c8b3501f131617f476f5fd7ae1e98cfc
wutianhang <mr_wu163@163.com>
2023-09-20 15:35:51 +0800

    测试redis存取

--- springboot-demo/src/main/java/com/icss/etc/controller/RedisController.java
+++ springboot-demo/src/main/java/com/icss/etc/controller/RedisController.java
@@ -1,13 +1,16 @@
 package com.icss.etc.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.icss.etc.pojo.User;
 import com.icss.etc.service.RedisService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +24,8 @@ public class RedisController {
 
     @Autowired
     private RedisService redisService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping(value = "/setString/{key}/{value}")
     @ApiOperation(value = "restful风格传参测试Redis存数据")
@@ -31,7 +36,7 @@ public class RedisController {
     @GetMapping(value = "/setString")
     @ApiOperation(value = "测试Redis存数据")
     public String setString(@RequestParam(value = "key", required = false) String key,
-                          @RequestParam(value = "value", required = false) String value) {
+                            @RequestParam(value = "value", required = false) String value) {
         logger.info("测试Redis存数据", key, value);
         return redisService.setString(key, value);
     }
@@ -43,5 +48,42 @@ public class RedisController {
         logger.info("传入的key值：" + key);
         logger.info("获取到的name值：" + name);
         return name;
+    }
+
+    @GetMapping("/setPojo")
+    @ApiOperation(value = "测试存对象")
+    public String setPojo() {
+        logger.info("测试存对象");
+        User user = new User("11", "zhangsan", "111222", "nv", "15998777777");
+
+        // 如果是个实体，我们可以使用json工具转成json字符串
+        redisService.setString("user", JSON.toJSONString(user));
+        logger.info("用户信息：" + redisService.getString("user"));
+        return redisService.getString("user");
+    }
+
+    @GetMapping("/setGetHash")
+    @ApiOperation(value = "测试存取Hash")
+    public void setGetHash(String key, String filedKey, String value) {
+
+        HashOperations<String, Object, Object> hashOperations = stringRedisTemplate.opsForHash();
+        hashOperations.put(key, filedKey, value);
+        logger.info("存入Hash成功");
+
+        String returnValue = (String) stringRedisTemplate.opsForHash().get(key, filedKey);
+        logger.info("取出的Hash：" + returnValue);
+    }
+
+    @GetMapping("/setGetList")
+    @ApiOperation(value = "测试存取List")
+    public void setGetList() {
+        ListOperations<String, String> listOperations = stringRedisTemplate.opsForList();
+        listOperations.leftPush("list", "value1");
+        listOperations.leftPush("list", "value2");
+        listOperations.leftPush("list", "value3");
+        logger.info("存入成功");
+
+        List<String> list = stringRedisTemplate.opsForList().range("list", 0, -1);
+        logger.info("取出的list " + list);
     }
 }
--- springboot-demo/src/main/java/com/icss/etc/service/impl/RedisServiceImpl.java
+++ springboot-demo/src/main/java/com/icss/etc/service/impl/RedisServiceImpl.java
@@ -1,6 +1,8 @@
 package com.icss.etc.service.impl;
 
 import com.icss.etc.service.RedisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -9,15 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class RedisServiceImpl implements RedisService {
 
+    private static Logger logger = LoggerFactory.getLogger(RedisServiceImpl.class);
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     /**
      * set redis: string类型
-     * @param key key
+     *
+     * @param key   key
      * @param value value
      */
-    public String setString(String key, String value){
+    public String setString(String key, String value) {
         ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         valueOperations.set(key, value);
         return "存入成功";
@@ -25,10 +30,11 @@ public class RedisServiceImpl implements RedisService {
 
     /**
      * get redis: string类型
+     *
      * @param key key
      * @return
      */
-    public String getString(String key){
+    public String getString(String key) {
         return stringRedisTemplate.opsForValue().get(key);
     }
 }
