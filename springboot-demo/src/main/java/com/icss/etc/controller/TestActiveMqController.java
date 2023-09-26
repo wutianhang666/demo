afa86cbaa985a5197bfcb27bd06138129accd4db
wutianhang <mr_wu163@163.com>
2023-09-26 14:58:12 +0800

    优化

--- springboot-demo/src/main/java/com/icss/etc/controller/TestActiveMqController.java
+++ springboot-demo/src/main/java/com/icss/etc/controller/TestActiveMqController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
