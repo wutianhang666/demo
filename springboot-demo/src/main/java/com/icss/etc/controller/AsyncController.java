6d36b05615d9fb0250bf4708ef8345d6acc6b9f3
wutianhang <mr_wu163@163.com>
2024-01-25 09:50:44 +0800

    测试异步任务

---
+++ springboot-demo/src/main/java/com/icss/etc/controller/AsyncController.java
@@ -0,0 +1,27 @@
+package com.icss.etc.controller;
+
+import com.icss.etc.service.AsyncService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "异步功能测试")
+@RequestMapping("/async")
+public class AsyncController {
+
+    @Autowired
+    private AsyncService asyncService;
+
+    @GetMapping("/async")
+    @ApiOperation(value = "测试异步执行")
+    public String async(){
+        asyncService.testAsync();
+
+        System.out.println("开启异步执行");
+        return "ok";
+    }
+}
