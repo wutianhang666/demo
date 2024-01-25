fb87f708881ccab85c966e93b4e90ec4ca783ac1
wutianhang <mr_wu163@163.com>
2024-01-25 09:50:44 +0800

    测试异步任务

---
+++ springboot-demo/src/main/java/com/icss/etc/service/impl/AsyncServiceImpl.java
@@ -0,0 +1,25 @@
+package com.icss.etc.service.impl;
+
+import com.icss.etc.service.AsyncService;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsyncServiceImpl implements AsyncService {
+
+    /**
+     * @Async注解的方法，称之为异步方法，该方法会在执行时，会单独开启一个线程来执行
+     * 去springboot主程序中开启异步注解功能  @EnableAsync
+     */
+
+    @Override
+    @Async
+    public void testAsync() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("数据正在传输...");
+    }
+}
