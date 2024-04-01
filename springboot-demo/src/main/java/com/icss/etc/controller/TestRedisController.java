a566dfc568d72a82a58b2995e940ce5fb9386920
wutianhang <mr_wu163@163.com>
2024-04-01 22:39:21 +0800

    测试redis

--- springboot-demo/src/main/java/com/icss/etc/controller/TestRedisController.java
+++ springboot-demo/src/main/java/com/icss/etc/controller/TestRedisController.java
@@ -2,7 +2,8 @@ package com.icss.etc.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.icss.etc.pojo.User;
-import com.icss.etc.service.TestRedisService;
+import com.icss.etc.service.RedisService;
+import com.icss.etc.utils.RedisUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -20,12 +21,12 @@ import java.util.List;
 @RestController
 @Api(value = "Redis功能测试", description = "Redis功能测试")
 @RequestMapping("/user")
-public class TestRedisController {
+public class RedisController {
 
-    private static Logger logger = LoggerFactory.getLogger(TestRedisController.class);
+    private static Logger logger = LoggerFactory.getLogger(RedisController.class);
 
     @Autowired
-    private TestRedisService redisService;
+    private RedisService redisService;
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -110,6 +111,10 @@ public class TestRedisController {
     public String setPojo2() {
         User user = new User("1", "zhangsan2", "111222", "nv", "15998777777");
         redisTemplate.opsForValue().set("user", user);
+
+        System.out.println("存入成功");
+        System.out.println(redisTemplate.opsForValue().get("user"));
+        ;
         return "成功";
     }
 
@@ -143,4 +148,16 @@ public class TestRedisController {
         jedis.set("name", "xiaotian666");
         System.out.println(jedis.get("name"));
     }
+
+    //===============使用redis工具类操作================================================
+
+    @GetMapping(value = "/testUtils/{name}")
+    @ApiOperation(value = "测试使用自定义redis工具类，判断键是否存在")
+    public Object testUtils(@PathVariable(value = "name") String name) {
+
+        boolean b = redisUtil.hasKey(name);
+        System.out.println(b);
+        return b;
+    }
+
 }
