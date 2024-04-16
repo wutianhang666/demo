a95297af43eeae567d1e9655916b173cb71f1c0b
wutianhang <mr_wu163@163.com>
2024-04-16 22:52:40 +0800

    菜单管理

---
+++ springboot-demo/src/main/java/com/icss/etc/mapper/SysMenuMapper.java
@@ -0,0 +1,11 @@
+package com.icss.etc.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.icss.etc.pojo.SysMenu;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface SysMenuMapper extends BaseMapper<SysMenu> {
+
+
+}
