2e6551fb5aec9755a6f417bbcab3328550cb7a6d
wutianhang <mr_wu163@163.com>
2024-04-18 22:59:23 +0800

    添加登录token处理

---
+++ springboot-demo/src/main/java/com/icss/etc/service/SysUserService.java
@@ -0,0 +1,7 @@
+package com.icss.etc.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.icss.etc.pojo.SysUser;
+
+public interface SysUserService extends IService<SysUser> {
+}
