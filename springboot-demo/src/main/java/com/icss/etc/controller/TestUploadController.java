1c4daf35cdc155ce6e48036af2edbb8d3a35166d
wutianhang <mr_wu163@163.com>
2024-04-29 13:51:49 +0800

    优化文件上传

--- springboot-demo/src/main/java/com/icss/etc/controller/TestUploadController.java
+++ springboot-demo/src/main/java/com/icss/etc/controller/TestUploadController.java
@@ -1,6 +1,8 @@
 package com.icss.etc.controller;
 
+import com.icss.etc.common.BaseResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -12,19 +14,26 @@ import java.io.IOException;
 
 @RestController
 @Api(value = "测试文件上传，展示", description = "测试文件上传，展示")
-@RequestMapping("/upload")
+@RequestMapping("/testUpload")
 public class TestUploadController {
 
     //上传头像
     @PostMapping("/upload")
-    public Object upload(@RequestPart MultipartFile multipartFile) throws IOException {
+    @ApiOperation(value = "文件上传")
+    public Object upload(@RequestPart(value = "file") MultipartFile file) throws IOException {
 
-        //获取图片名称
-        System.out.println(multipartFile.getOriginalFilename());
-        //将上传的图片保存
-        multipartFile.transferTo(new File("E:/ceshi/"+multipartFile.getOriginalFilename()));
-
-        return "success 上传成功";
+        try {
+            //获取图片名称
+            String fileName = file.getOriginalFilename();
+            // 指定保存路径
+            String filePath = "D:/photo/" + fileName;
+            //将文件保存到本地
+            file.transferTo(new File(filePath));
+            return BaseResult.success("操作成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return BaseResult.failResultCreate("上传失败");
+        }
     }
 
 }
